3d17f22e6d47a16b1e3df7d8e41e82e365a06706
valentinzector <valentin@zector.com.ar>
2023-04-02 17:31:46 -0300

    user controlador add test function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 class UserController extends Controller
 {
-    //
+    public function auth_user(Request $request): RedirectResponse
+    {
+        $credenciales = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        //Recordarme
+        if(Auth::viaRemember()){
+            // test
+        }
+        if(Auth::attempt($credenciales)){
+            $request->session()->regenerate();
+            return redirect()->intended();
+        }
+        return back()->withErrors([
+            'email' => 'Las credenciales ingresadas no coinciden con nuestros registros.'
+        ])->onlyInput('email');
+    }
 }

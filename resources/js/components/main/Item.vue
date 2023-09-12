b2d89684e3a45f1b755498a7cac375b051e9b265
Daniel Cantor <daniel.cantor2019@gmail.com>
2023-09-12 16:24:26 -0300

    update

--- resources/js/components/main/Item.vue
+++ resources/js/components/main/Item.vue
@@ -4,10 +4,6 @@
             <img src="img/home/ninos.png" class="px-3 pt-3 rounded-0" >
             <div class="p-0">
             <h5 class="text-center p-3 text-light fs-6 position-absolute  start-50 translate-middle w-75" style="background-color:rgb(150, 131, 236); font-family:'poppins';border-color:rgb(150, 131, 236);">Republica de los niños</h5>
-            <p class="text-center mt-5 lh-1 mx-5 fs-1 " style="border:1px solid #f18701; border-top:0; border-right:0; border-left:0;  ">
-                <span class="text-uppercase" style="font-family:'Raleway', sans-serif;color:#2e005d;">desde</span>
-                <span class="fw-bolder text-uppercase" style="font-family:'poppins';color:#f18701">usd1220</span>
-            </p>
             <p class="text-center">
                 <span class="fs-6" style="font-family:'Raleway', sans-serif;color:#2e005d;"> <img src="/img/icons/calendario.svg" width='5%'> 8 dias , 7 noches</span>
             </p>

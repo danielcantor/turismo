909173f2a00213521c63524476df610cce332830
Daniel Cantor <daniel.cantor2019@gmail.com>
2023-09-20 12:06:14 -0300

    update

--- resources/js/components/turismo/Index.vue
+++ resources/js/components/turismo/Index.vue
@@ -11,13 +11,11 @@
                                 <p class="fw-bolder" style="font-family:poppins;font-size:3.5rem;">Experiencia</p> 
                             </h4>
                             <p class="text-justify my-4" style="font-family:'Raleway', sans-serif;">
-                                Somos a una empresa familiar y profesional dedicados a cumplir los sueños de nuestros pasajeros, en cada viaje brindamos una óptima calidad de servicio tratando de cumplir con las expectativas de cada uno de los que confía en nuestro trabajo.
-                                Agradecemos infinitamente a quienes estuvieron a nuestro lado durante tantos años de trabajo, apoyándonos para que hoy en día podamos nosotros cumplir nuestro sueño!                            
+                                {{descripcion}}
                             </p>
                         </div>
                     </div>
                     <div class="col-xl-6 text-center order-1 order-xl-2">
-                        <img src="/img/home/Sobre-nosotros.jpg" alt="" width="75%" class="shadow-image">
                     </div>
                 </div>
             </div>
@@ -48,7 +46,8 @@
                 products: window.posts,
                 imageUrl : window.imageUrl,
                 title: window.title,
-                subtitle: window.subtitle
+                subtitle: window.subtitle,
+                descripcion: window.descripcion,
             }
         },
         mounted() {

d8e751763f46063ab66ea2e8297b33304fa8fc56
xzerothx <valeendiz@gmail.com>
2023-11-01 10:53:08 -0300

    test

--- resources/views/productos/store_product.blade.php
+++ resources/views/productos/store_product.blade.php
@@ -75,7 +75,8 @@
                                     <a href="whatsapp://send?text=Te comparto la información sobre este viaje! <?=$_SERVER["HTTP_HOST"].$_SERVER["REQUEST_URI"]?>" target="_blank" class="heading__share__item__label"><h5><i class="fas fa-share-alt"></i> Compartir</h5></a>
                                 </div>
                                 <div class="col-5 titulo_producto">
-                                    <button class="btn btn-primary" @click="openModal">CONDICIONES GENERALES</button>
+                                    <modal-example></modal-example>
+                                    
                                 </div>
                             </div>
                         </div>
@@ -123,24 +124,7 @@
         </section>
          
         <hr>
-        <div class="modal" tabindex="-1" role="dialog" :class="{ 'show': showModal }">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title">Imagen</h5>
-                    <button type="button" class="close" @click="openModal">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-                <div class="modal-body">
-                    <img src="/img/home/condicionesgenerales.jpeg" alt="Imagen Modal">
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" @click="openModal">Cerrar</button>
-                </div>
-                </div>
-            </div>
-        </div>
+
         <footer-component></footer-component>
     </div>
     
@@ -160,6 +144,26 @@
                     this.showModal = false;
                 },
             },
+            template: `
+            <button class="btn btn-primary" @click="openModal">CONDICIONES GENERALES</button>
+                <div class="modal" tabindex="-1" role="dialog" :class="{ 'show': showModal }">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title">Imagen</h5>
+                            <button type="button" class="close" @click="openModal">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <img src="/img/home/condicionesgenerales.jpeg" alt="Imagen Modal">
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" @click="openModal">Cerrar</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>`
         });
 
         new Vue({

126abf2e35ae20540f7ecef2258b768341719178
valentinzector <valentin@zector.com.ar>
2023-11-03 12:00:25 -0300

    update

--- public/js/app.js
+++ public/js/app.js
@@ -6796,7 +6796,6 @@ __webpack_require__.r(__webpack_exports__);
 //
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
-  name: 'modal-example',
   data: function data() {
     return {
       showModal: false
--- resources/js/components/turismo/Condiciones.vue
+++ resources/js/components/turismo/Condiciones.vue
@@ -24,7 +24,6 @@
   
   <script>
   export default {
-    name: 'modal-example',
     data() {
       return {
         showModal: false,

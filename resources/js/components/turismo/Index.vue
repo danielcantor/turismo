09e6603d626691ec51a9b0b457e35a3ed6f3887a
Daniel Cantor <daniel.cantor2019@gmail.com>
2025-10-20 19:48:00 -0300

    Refactor: pass data to Turismo component via props and update Index.vue to use props (remove window globals)

--- resources/js/components/turismo/Index.vue
+++ resources/js/components/turismo/Index.vue
@@ -1,58 +1,76 @@
 <template>
-    <section>
-        <Slider :imageUrl="imageUrl"/>
-        <div class="py-5" style="background-color: #f6f6f6;">
-            <div class="container mt-5">
-                <div class="row justify-content-center">
-                    <div class="col-xl-6 order-2 order-xl-1">
-                        <div class="mb-5">
-                            <h4 class="text-left title-custom border-custom ps-3 lh-1"> 
-                                <p class=' mb-0' style="font-family:cherolinaregular;font-size:3.9rem;">Vive una gran</p> 
-                                <p class="fw-bolder" style="font-family:poppins;font-size:3.5rem;">Experiencia</p> 
-                            </h4>
-                            <p class="text-justify my-4" style="font-family:'Raleway', sans-serif;">
-                                {{descripcion}}
-                            </p>
-                        </div>
-                    </div>
-                    <div class="col-xl-6 text-center order-1 order-xl-2">
-                    </div>
-                </div>
+  <section>
+    <Slider :imageUrl="imageUrl" />
+    <div class="py-5" style="background-color: #f6f6f6;">
+      <div class="container mt-5">
+        <div class="row justify-content-center">
+          <div class="col-xl-6 order-2 order-xl-1">
+            <div class="mb-5">
+              <h4 class="text-left title-custom border-custom ps-3 lh-1">
+                <p class="mb-0" style="font-family:cherolinaregular;font-size:3.9rem;">Vive una gran</p>
+                <p class="fw-bolder" style="font-family:poppins;font-size:3.5rem;">Experiencia</p>
+              </h4>
+              <p class="text-justify my-4" style="font-family:'Raleway', sans-serif;">
+                {{ description }}
+              </p>
             </div>
+          </div>
+          <div class="col-xl-6 text-center order-1 order-xl-2">
+          </div>
         </div>
-        <div class="container py-3">
-            <div class="row justify-content-center align-items-center">
-                    <h4 class=" col-12 text-center title-custom ps-3 lh-1"> 
-                        <p class=' mb-0' style="font-family:cherolinaregular;font-size:3.9rem;">{{title}}</p> 
-                        <p class="fw-bolder" style="font-family:poppins;font-size:3.5rem;">{{subtitle}}</p> 
-                    </h4>
-                    <div v-if="!products.length" class="col-12 text-center py-5">
-                        <i class="fa-solid fa-triangle-exclamation fa-3x mb-2"></i>
-                        <h4 class="text-center">No hay productos en esta categoria</h4>
-                    </div>
-                    <Item v-for="(product, index) in products" :key="product.id" v-bind="product" />
-            </div>
-        </div>
-    </section>
-</template>
-    
-    <script>
-    import Slider from '../main/Slider.vue';
-    import Item from '../main/Item.vue';
-    export default {      
-        components: { Slider , Item},  
-        data(){
-            return {
-                products: window.posts,
-                imageUrl : window.imageUrl,
-                title: window.title,
-                subtitle: window.subtitle,
-                descripcion: window.description,
-            }
-        },
-        mounted() {
+      </div>
+    </div>
 
-        }
-        }
-    </script>
-    
+    <div class="container py-3">
+      <div class="row justify-content-center align-items-center">
+        <h4 class="col-12 text-center title-custom ps-3 lh-1">
+          <p class="mb-0" style="font-family:cherolinaregular;font-size:3.9rem;">{{ title }}</p>
+          <p class="fw-bolder" style="font-family:poppins;font-size:3.5rem;">{{ subtitle }}</p>
+        </h4>
+
+        <div v-if="!posts.length" class="col-12 text-center py-5">
+          <i class="fa-solid fa-triangle-exclamation fa-3x mb-2"></i>
+          <h4 class="text-center">No hay productos en esta categoria</h4>
+        </div>
+
+        <Item v-for="product in posts" :key="product.id" v-bind="product" />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Slider from '../main/Slider.vue';
+import Item from '../main/Item.vue';
+
+export default {
+  name: 'TurismoIndex',
+  components: { Slider, Item },
+  props: {
+    posts: {
+      type: Array,
+      default: () => []
+    },
+    imageUrl: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    },
+    description: {
+      type: String,
+      default: ''
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Recomendación: mover estilos inline a SCSS/CSS compartido para mantenimiento */
+</style>
--- resources/views/turismo.blade.php
+++ resources/views/turismo.blade.php
@@ -11,14 +11,6 @@
     <link rel="apple-touch-icon" sizes="72x72" href="{{ url('/apple-icon-72x72.png') }}">
     <link rel="apple-touch-icon" sizes="76x76" href="{{ url('/apple-icon-76x76.png') }}">
     <link rel="apple-touch-icon" sizes="114x114" href="{{ url('/apple-icon-114x114.png') }}">
-    <link rel="apple-touch-icon" sizes="120x120" href="{{ url('/apple-icon-120x120.png') }}">
-    <link rel="apple-touch-icon" sizes="144x144" href="{{ url('/apple-icon-144x144.png') }}">
-    <link rel="apple-touch-icon" sizes="152x152" href="{{ url('/apple-icon-152x152.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ url('/apple-icon-180x180.png') }}">
-    <link rel="icon" type="image/png" sizes="192x192"  href="{{ url('/android-icon-192x192.png') }}">
-    <link rel="icon" type="image/png" sizes="32x32" href="{{ url('/favicon-32x32.png') }}">
-    <link rel="icon" type="image/png" sizes="96x96" href="{{ url('/favicon-96x96.png') }}">
-    <link rel="icon" type="image/png" sizes="16x16" href="{{ url('/favicon-16x16.png') }}">
     <link rel="manifest" href="{{ url('/manifest.json') }}">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="{{ url('/ms-icon-144x144.png') }}">
@@ -29,16 +21,19 @@
 <body>
     <div id="app">
         <header-component></header-component>
-        <turismo-component></turismo-component>
-        <footer-component></footer-component>    
+
+        <!-- Pasamos datos al componente Vue mediante props en vez de window.* -->
+        <turismo-component
+            :posts='@json($items)'
+            :image-url='@json(Storage::url($imageUrl))'
+            :title='@json($title)'
+            :subtitle='@json($subtitle)'
+            :description='@json($description)'>
+        </turismo-component>
+
+        <footer-component></footer-component>
     </div>
-    <script>
-        window.posts = @json($items);
-        window.imageUrl = '{{ $imageUrl }}';
-        window.title = '{{ $title }}';
-        window.subtitle = '{{ $subtitle }}';
-        window.description = '{{ $description }}';
-    </script>
+
     <script src="{{ mix('/js/app.js') }}"></script>
 </body>
 </html>
